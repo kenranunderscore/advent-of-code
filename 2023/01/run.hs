d20b06bd675788b7ad1e21e219dc18287b3b7606
Johannes Maier <johannes.maier@mailbox.org>
2023-12-01 17:43:15 +0100

    2023: day 1, part 2

--- 2023/01/run.hs
+++ 2023/01/run.hs
@@ -1,19 +1,46 @@
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE MultiWayIf #-}
+{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
+import Data.List
 import Control.Monad
 import Data.Char
+import Data.Function ((&))
 
-part1 :: IO ()
-part1 = do
-  input <- readFile "./input"
-  let ls = lines input
-      ns = fmap
+part1 :: [String] -> Int
+part1 ls = do
+  let ns = fmap
                (\line ->
                    let digits = filter isDigit line
-                   in read [head digits, last digits])
+                   in read @Int [head digits, last digits])
                ls
-  print $ sum ns
+  sum ns
+
+-- | HAHAHA this is the ugliest solution ever
+transform :: String -> String
+transform = \case
+  [] -> []
+  s@(x:xs) ->
+    if
+      | "one" `isPrefixOf` s -> "1" <> transform (drop (length "one" - 1) s)
+      | "two" `isPrefixOf` s -> "2" <> transform (drop (length "two" - 1) s)
+      | "three" `isPrefixOf` s -> "3" <> transform (drop (length "three" - 1) s)
+      | "four" `isPrefixOf` s -> "4" <> transform (drop (length "four" - 1) s)
+      | "five" `isPrefixOf` s -> "5" <> transform (drop (length "five" - 1) s)
+      | "six" `isPrefixOf` s -> "6" <> transform (drop (length "six" - 1) s)
+      | "seven" `isPrefixOf` s -> "7" <> transform (drop (length "seven" - 1) s)
+      | "eight" `isPrefixOf` s -> "8" <> transform (drop (length "eight" - 1) s)
+      | "nine" `isPrefixOf` s -> "9" <> transform (drop (length "nine" - 1) s)
+      | otherwise -> x : transform xs
+
+part2 :: [String] -> Int
+part2 ls = do
+  let transformed = fmap transform ls
+  part1 transformed
 
 main :: IO ()
 main = do
-  part1
+  ls <- lines <$> readFile "input"
+  print $ part1 ls
+  print $ part2 ls
