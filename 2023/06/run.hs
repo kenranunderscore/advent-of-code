b66f968ec9ec544b5c2c460a6690a7c429f6bfd0
Johannes Maier <johannes.maier@mailbox.org>
2023-12-11 13:40:42 +0100

    2023/06.2: solve

--- 2023/06/run.hs
+++ 2023/06/run.hs
@@ -15,6 +15,7 @@ import Data.Foldable (foldl')
 import Data.List
 import Data.Maybe (mapMaybe)
 import Data.Text (Text)
+import Data.Text qualified as T
 import Data.Text.IO qualified as T
 import Debug.Trace
 import GHC.Generics (Generic)
@@ -41,6 +42,18 @@ data Race = Race
     }
     deriving stock (Show)
 
+part2Race :: Parser Race
+part2Race = do
+    "Time:" >> skipSpace
+    t <- num
+    "Distance:" >> skipSpace
+    r <- num
+    pure $ Race t r
+  where
+    num = do
+        cs <- manyTill' anyChar endOfLine
+        pure $ read $ filter (/= ' ') cs
+
 races :: Parser [Race]
 races = do
     ts <- times
@@ -50,18 +63,23 @@ races = do
 
 beatsRecord :: Race -> Time -> Bool
 beatsRecord race secondsHeld =
-  let distance = (race.duration - secondsHeld) * secondsHeld
-   in distance > race.record
+    let distance = (race.duration - secondsHeld) * secondsHeld
+    in distance > race.record
 
 part1 :: [Race] -> Int
 part1 rs =
     product $ fmap strategies rs
-  where
-    strategies race =
-      length $ filter (beatsRecord race) [1 .. race.duration - 1]
+
+strategies :: Race -> Int
+strategies race = length $ filter (beatsRecord race) [1 .. race.duration - 1]
+
+part2 :: Race -> Int
+part2 = strategies
 
 main :: IO ()
 main = do
     input <- T.readFile "input"
     let Right rs = parseOnly races input
     print $ part1 rs
+    let Right r = parseOnly part2Race input
+    print $ part2 r
