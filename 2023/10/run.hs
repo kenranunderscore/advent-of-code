0b2e91810a17d029e3f8e78fd8923e077cce3167
Johannes Maier <johannes.maier@mailbox.org>
2023-12-11 08:10:41 +0100

    2023/10.1: refactor to prepare for part 2

--- 2023/10/run.hs
+++ 2023/10/run.hs
@@ -9,6 +9,7 @@
 import Control.Applicative
 import Data.Attoparsec.Text
 import Data.Foldable (foldl')
+import Data.Function ((&))
 import Data.Functor
 import Data.List qualified as List
 import Data.Maybe qualified as Maybe
@@ -19,6 +20,7 @@ import Data.Text qualified as T
 import Data.Text.IO qualified as T
 import Data.Vector (Vector, (!))
 import Data.Vector qualified as V
+import Debug.Trace
 
 data Tile = TopBottom | LeftRight | BottomLeft | BottomRight | TopLeft | TopRight | Start | Empty
     deriving stock (Eq)
@@ -34,15 +36,19 @@ instance Show Tile where
         BottomLeft -> "╚"
         BottomRight -> "╝"
 
-newtype Area = Area {value :: Vector (Vector Tile)}
+newtype Area a = Area {value :: Vector (Vector a)}
 
-instance Show Area where
+instance Show (Area Tile) where
     show (Area rows) =
         List.intercalate "\n" (V.toList $ fmap showRow rows)
       where
         showRow row = concat $ V.toList $ fmap show row
 
-area :: Parser Area
+instance Show (Area Char) where
+    show (Area rows) =
+        List.intercalate "\n" (V.toList $ fmap V.toList rows)
+
+area :: Parser (Area Tile)
 area = do
     Area . V.fromList <$> many1 row
   where
@@ -57,61 +63,82 @@ area = do
             <|> ("F" >> pure TopLeft)
     row = V.fromList <$> (many1' tile <* char '\n')
 
-areaHeight :: Area -> Int
+areaHeight :: Area a -> Int
 areaHeight = length . (.value)
 
-areaWidth :: Area -> Int
+areaWidth :: Area a -> Int
 areaWidth = length . V.head . (.value)
 
 type Pos = (Int, Int)
 
-tileAt :: Area -> Pos -> Tile
+tileAt :: Area a -> Pos -> a
 tileAt (Area area) (y, x) =
     area ! y ! x
 
-start :: Area -> Pos
-start (Area area) =
+start' :: (a -> Bool) -> Area a -> Pos
+start' pred area =
     let
-        xs = fmap (V.findIndex (== Start)) area
+        xs = fmap (V.findIndex pred) area.value
         Just y = V.findIndex Maybe.isJust xs
         Just x = xs ! y
     in
         (y, x)
 
-connectedTiles :: Area -> Pos -> Set Pos
-connectedTiles area pos@(y, x) =
-    let
-        origin = tileAt area pos
-        tiles =
-            [ if y - 1 >= 0
-                && let t = tileAt area (y - 1, x)
-                   in (origin == Start || origin == BottomLeft || origin == BottomRight || origin == TopBottom)
-                        && (t == TopBottom || t == TopLeft || t == TopRight)
-                then Just (y - 1, x)
-                else Nothing
-            , if y + 1 < areaHeight area
-                && let t = tileAt area (y + 1, x)
-                   in (origin == Start || origin == TopBottom || origin == TopLeft || origin == TopRight)
-                        && (t == BottomLeft || t == BottomRight || t == TopBottom)
-                then Just (y + 1, x)
-                else Nothing
-            , if x - 1 >= 0
-                && let t = tileAt area (y, x - 1)
-                   in (origin == Start || origin == LeftRight || origin == TopRight || origin == BottomRight)
-                        && (t == LeftRight || t == TopLeft || t == BottomLeft)
-                then Just (y, x - 1)
-                else Nothing
-            , if x + 1 < areaWidth area
-                && let t = tileAt area (y, x + 1)
-                   in (origin == Start || origin == LeftRight || origin == TopLeft || origin == BottomLeft)
-                        && (t == LeftRight || t == TopRight || t == BottomRight)
-                then Just (y, x + 1)
-                else Nothing
-            ]
-    in
-        Set.fromList $ Maybe.catMaybes tiles
+start :: Area Tile -> Pos
+start = start' (== Start)
 
-part1 :: Area -> Int
+insideBounds :: Area a -> Pos -> Bool
+insideBounds area (y, x) =
+    y >= 0 && y < h && x >= 0 && x < w
+  where
+    w = areaWidth area
+    h = areaHeight area
+
+connected' ::
+    Eq a =>
+    a ->
+    a ->
+    a ->
+    a ->
+    a ->
+    a ->
+    a ->
+    Area a ->
+    Pos ->
+    Pos ->
+    Bool
+connected' s tb lr tl tr bl br area p@(y, x) p2@(y2, x2)
+    | not (insideBounds area p2) = False
+    | y2 == y && x2 == x - 1 =
+        (origin == s || origin == lr || origin == tr || origin == br)
+            && (t == lr || t == tl || t == bl)
+    | y2 == y && x2 == x + 1 =
+        traceShowId $
+            (origin == s || origin == lr || origin == tl || origin == bl)
+                && (t == lr || t == tr || t == br)
+    | y2 == y - 1 && x2 == x =
+        (origin == s || origin == bl || origin == br || origin == tb)
+            && (t == tb || t == tl || t == tr)
+    | y2 == y + 1 && x2 == x =
+        (origin == s || origin == tb || origin == tl || origin == tr)
+            && (t == bl || t == br || t == tb)
+    | otherwise = False
+  where
+    t = tileAt area p2
+    origin = tileAt area p
+
+connected :: Area Tile -> Pos -> Pos -> Bool
+connected = connected' Start TopBottom LeftRight TopLeft TopRight BottomLeft BottomRight
+
+connectedTiles :: Area Tile -> Pos -> Set Pos
+connectedTiles area pos@(y, x) =
+    tiles
+        & filter (connected area pos)
+        & Set.fromList
+  where
+    tiles = [(y - 1, x), (y + 1, x), (y, x - 1), (y, x + 1)]
+
+part1 :: Area Tile -> Int
 part1 area =
     1 + go (Set.singleton (start area)) mempty 0
   where
