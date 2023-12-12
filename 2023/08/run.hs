0ec5416a3ace7f1cf0c2197e48eff110a3a00d65
Johannes Maier <johannes.maier@mailbox.org>
2023-12-12 19:42:57 +0100

    2023/08.2: solve

--- 2023/08/run.hs
+++ 2023/08/run.hs
@@ -3,18 +3,24 @@
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE OverloadedRecordDot #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE Strict #-}
 {-# LANGUAGE NoFieldSelectors #-}
 
 import Control.Applicative
+import Control.Concurrent
 import Control.Concurrent.Async qualified as A
+import Control.Concurrent.MVar
+import Control.Monad
 import Data.Attoparsec.Text
 import Data.Foldable (foldl')
 import Data.Functor
-import Data.Map.Strict (Map)
+import Data.List.NonEmpty qualified as NE
+import Data.Map.Strict (Map, (!))
 import Data.Map.Strict qualified as M
 import Data.Maybe (mapMaybe)
 import Data.Set (Set)
 import Data.Set qualified as Set
+import Data.Set.Internal qualified as Set
 import Data.Text (Text)
 import Data.Text qualified as T
 import Data.Text.IO qualified as T
@@ -73,10 +79,32 @@ part1 instrs net =
 endsWith :: Char -> Node -> Bool
 endsWith c node = last (T.unpack node.value) == c
 
+zloopFinder :: Instructions -> Network -> Node -> Int
+zloopFinder instrs net =
+    go instrs 1 Nothing
+  where
+    go (i : rest) steps zprev curr = do
+        let next = advance net i curr
+        if endsWith 'Z' next
+            then case zprev of
+                Nothing ->
+                    go rest (steps + 1) (Just (next, steps)) next
+                Just (target, n) ->
+                    steps - n
+            else go rest (steps + 1) zprev next
+
+-- I don't like part 2... this solution only works under specific circumstances
+part2 :: Instructions -> Network -> Int
+part2 instrs net = do
+    let
+        startNodes = filter (endsWith 'A') $ M.keys net
+        (x : xs) = fmap (zloopFinder instrs net) startNodes
+     in
+        foldl lcm x xs
+
 main :: IO ()
 main = do
     input <- T.readFile "input"
     let Right (i, n) = parseOnly instructionsAndNetwork input
-    print $ part1 i n
-
--- print $ part2 i n
+    -- print $ part1 i n
+    print $ part2 (cycle i) n
