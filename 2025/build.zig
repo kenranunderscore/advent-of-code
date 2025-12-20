e0f9a2f306171d2a0fe6586777e8f4d436f1d654
Johannes Maier <johannes.maier@mailbox.org>
2025-12-20 16:25:31 +0100

    2025: dynamically determine test targets in build.zig

--- 2025/build.zig
+++ 2025/build.zig
@@ -4,8 +4,23 @@ pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    var days = std.ArrayList(usize).empty;
+    defer days.deinit(b.allocator);
+
+    const src_dir = try std.fs.cwd().openDir("src", .{ .iterate = true });
+    var iter = src_dir.iterate();
+    while (try iter.next()) |entry| {
+        if (entry.kind == .file and
+            std.mem.startsWith(u8, entry.name, "day") and
+            std.mem.endsWith(u8, entry.name, ".zig"))
+        {
+            const day = try std.fmt.parseInt(usize, entry.name[3 .. entry.name.len - 4], 10);
+            try days.append(b.allocator, day);
+        }
+    }
+
     const test_all = b.step("test", "Run tests for all AoC days");
-    for (1..7) |day| {
+    for (days.items) |day| {
         const day_name = try std.fmt.allocPrint(b.allocator, "day{d}", .{day});
         const path = try std.fmt.allocPrint(b.allocator, "src/{s}.zig", .{day_name});
         const tests = b.addTest(.{
